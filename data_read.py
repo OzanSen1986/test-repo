72dd88664303d933007862cd3d7c93044fc417d5
Ozan Şen <ozanokan1903@gmail.com>
2025-10-15 18:04:24 +0300

    File read-write operations added.

---
+++ data_read.py
@@ -0,0 +1,13 @@
+with open('car.jpg', 'rb') as rf:
+    with open('car_copy.jpg', 'wb') as wf:
+        chunk_size = 4096
+        rf_chunk = rf.read(chunk_size)
+        while len(rf_chunk) > 0:
+            wf.write(rf_chunk)
+            rf_chunk = rf.read(chunk_size)
+
+    
+
+
+
+
